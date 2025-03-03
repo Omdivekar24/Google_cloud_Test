bac7153d634efd64b7866d284fd8efd77ea406b7
Omdivekar24 <135492879+Omdivekar24@users.noreply.github.com>
2025-03-03 11:37:17 +0530

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,5 @@
+print("Hello Welcome")
+print("How are u")
+print("Welcome to Google Cloud Platform")
+print("Welcome to the Zeal College of engineering ")
+print("This is the Computer Department present in D building ")
